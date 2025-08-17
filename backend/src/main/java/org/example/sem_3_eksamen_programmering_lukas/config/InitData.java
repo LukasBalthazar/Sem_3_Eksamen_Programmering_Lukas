982da76083e98abd2e27d7883a4568be287855af
Lukas Balthazar <LBTurner@outlook.dk>
2025-08-17 16:52:32 +0200

    InitData.java and WebConfig.java template classes added.

---
+++ backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/config/InitData.java
@@ -0,0 +1,18 @@
+package org.example.sem_3_eksamen_programmering_lukas.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+    @Autowired
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+}
+
---
+++ backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.example.sem_3_eksamen_programmering_lukas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //.allowedOrigins("https://example.com") // or "*" if you're allowing all, but not recommended
+                .allowedOrigins("*") // or "*" if you're allowing all, but not recommended
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false); // Set to false if you don't need cookies/auth headers
+
+    }
+}
