e9a031dc67c6bfa6ff5263bee4174f9caac83840
Lukas Balthazar <LBTurner@outlook.dk>
2025-08-17 16:11:14 +0200

    Initial commit

---
+++ src/main/java/org/example/sem_3_eksamen_programmering_lukas/Sem3EksamenProgrammeringLukasApplication.java
@@ -0,0 +1,13 @@
+package org.example.sem_3_eksamen_programmering_lukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem3EksamenProgrammeringLukasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem3EksamenProgrammeringLukasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sem_3_eksamen_programmering_lukas/Sem3EksamenProgrammeringLukasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sem_3_eksamen_programmering_lukas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem3EksamenProgrammeringLukasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
