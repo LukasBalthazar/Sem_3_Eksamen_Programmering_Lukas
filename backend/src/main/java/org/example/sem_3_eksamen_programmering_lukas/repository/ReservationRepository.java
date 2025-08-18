67660e6a140292bc1b56e87a1328c9119e784b8e
Lukas Balthazar <LBTurner@outlook.dk>
2025-08-18 21:29:37 +0200

    ReservationRepository added

---
+++ backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package org.example.sem_3_eksamen_programmering_lukas.repository;
+
+import org.example.sem_3_eksamen_programmering_lukas.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    List<Reservation> findReservationByCustomerId(int customerId);
+    List<Reservation> findReservationByCustomerIdAndActiveTrue(int customerId);
+    Optional<Reservation> findByCustomerIdAndAlbumId(int customerId, int albumId);
+}
