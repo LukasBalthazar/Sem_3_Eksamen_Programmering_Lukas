0cb8a4501fe9d6bf9d97bd80e5a1baa7a79e50e0
Lukas Balthazar <LBTurner@outlook.dk>
2025-08-18 22:31:40 +0200

    initData for Customer added

--- backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/config/InitData.java
+++ backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/config/InitData.java
@@ -1,7 +1,9 @@
 package org.example.sem_3_eksamen_programmering_lukas.config;
 
 import org.example.sem_3_eksamen_programmering_lukas.model.Album;
+import org.example.sem_3_eksamen_programmering_lukas.model.Customer;
 import org.example.sem_3_eksamen_programmering_lukas.repository.AlbumRepository;
+import org.example.sem_3_eksamen_programmering_lukas.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,8 @@ public class InitData implements CommandLineRunner {
 
     @Autowired
     AlbumRepository albums;
+    @Autowired
+    CustomerRepository customers;
 
     @Override
     public void run(String... args) throws Exception {
@@ -19,6 +23,11 @@ public class InitData implements CommandLineRunner {
         a.setAvailable(true);
         //a.setStore(cph);
         albums.save(a);
+
+        Customer u = new Customer();
+        u.setName("Test User");
+        u.setEmail("test@example.com");
+        customers.save(u);
 
     }
 
