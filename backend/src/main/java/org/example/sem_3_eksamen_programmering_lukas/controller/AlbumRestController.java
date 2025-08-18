b7e6f76b9f9ce4b92235bcb2c0de28c05ce68148
Lukas Balthazar <LBTurner@outlook.dk>
2025-08-18 18:13:32 +0200

    AlbumRestController Album update, Album delete functionality added

--- backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/controller/AlbumRestController.java
+++ backend/src/main/java/org/example/sem_3_eksamen_programmering_lukas/controller/AlbumRestController.java
@@ -15,20 +15,53 @@ public class AlbumRestController {
     @Autowired AlbumRepository albums;
     //@Autowired StoreRepository;
 
+    //List
     @GetMapping("/")
     public List<Album> getAllAlbums(){
         return albums.findAll();
     }
 
+    //Get
     @GetMapping("/{id}")
     public Album getAlbumById(@PathVariable int id){
         return albums.findById(id).get();
     }
 
+    //Create
     @PostMapping("/")
     public ResponseEntity<Album> createAlbum(@RequestBody Album a) {
         Album saved = albums.save(a);
         return ResponseEntity.created(URI.create("/albums/" + saved.getId())).body(saved);
     }
 
+    //Update
+    @PutMapping("/{id}")
+    public Album updateAlbum(@PathVariable int id, @RequestBody Album in) {
+        Album a = albums.findById(id).get();
+        a.setTitle(in.getTitle());
+        a.setArtist(in.getArtist());
+        /*a.setAvailable(in.isAvailable());
+        if (in.getStore() != null) {
+            int sid = in.getStore().getId();
+            a.setStore(albums.findById(sid).orElseThrow());
+        }*/
+        return albums.save(a);
+    }
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAlbum(@PathVariable int id) {
+        albums.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+/*
+    // Assign album to store (Opgave-4A)
+    @PostMapping("/{albumId}/assign-store/{storeId}")
+    public Album assign(@PathVariable int albumId, @PathVariable int storeId){
+        Album a = albums.findById(albumId).orElseThrow();
+        a.setStore(stores.findById(storeId).orElseThrow());
+        return albums.save(a);
+    }
+
+ */
 }
